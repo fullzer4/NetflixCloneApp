21ab21148adf7eb3066ccc15e6211e4999cb1fbf
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-14 21:19:08 +0000

    new

--- backend/src/method.rs
+++ backend/src/method.rs
@@ -1,18 +1,26 @@
-use actix_web::{get, post, HttpResponse, Responder, web};
-use serde::de;
+use actix_web::{get, post, HttpResponse, Responder, web, error};
+use serde::Deserialize;
 
 #[get("/")]
 pub async fn greet() -> impl Responder {
     HttpResponse::Ok().body("See the docs to see how to consume!")
 }
 
-#[post("/login")]
-pub async fn login(info: web::ReqData<LoginInfo>) -> impl Responder {
-    HttpResponse::Ok().body("See the docs to see how to consume!")
-}
-
-#[derive(Clone, )]
+#[derive(Deserialize, Clone)]
 pub struct LoginInfo {
     email: String,
-    password: i32,
+    password: String,
+}
+
+#[post("/login")]
+pub async fn login(info: web::Query<LoginInfo>) -> Result<HttpResponse, actix_web::Error> {
+
+    let email: Option<String> = Some(info.email.clone());
+    let password: Option<String> = Some(info.password.clone());
+
+    if email.is_none() || password.is_none() {
+        return Err(error::ErrorBadRequest("verify all the request"));
+    }
+
+    Ok(HttpResponse::Ok().json(email))
 }
