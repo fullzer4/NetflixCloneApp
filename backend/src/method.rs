1f4b00963fdf5436acf9ca1b9ec06e791f8bad0d
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-14 19:49:39 +0000

    new

--- backend/src/method.rs
+++ backend/src/method.rs
@@ -1,5 +1,5 @@
 use actix_web::{get, post, HttpResponse, Responder, web};
-use serde::Deserialize;
+use serde::de;
 
 #[get("/")]
 pub async fn greet() -> impl Responder {
@@ -7,11 +7,11 @@ pub async fn greet() -> impl Responder {
 }
 
 #[post("/login")]
-pub async fn login(info: web::Query<LoginInfo>) -> impl Responder {
+pub async fn login(info: web::ReqData<LoginInfo>) -> impl Responder {
     HttpResponse::Ok().body("See the docs to see how to consume!")
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, )]
 pub struct LoginInfo {
     email: String,
     password: i32,
